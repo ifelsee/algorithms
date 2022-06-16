9d52304df16b2ddd6126862bba8f9aaefc3f58a3
Burak <49848935+ifelsee@users.noreply.github.com>
2022-06-16 12:56:38 +0300

    Update radix-sort0.py

--- radix-sort0.py
+++ radix-sort0.py
@@ -3,18 +3,21 @@
 # verimli kullanımı için bir ton optimizasyon yapılması gerek. üşengeç herifin önde gideni olduğum için
 # string olarak işledim veriyi...
 
-
 import random
+from typing import List
 
-l1 = [i for i in list(range(9001))]
-maxl = len(str(max(l1)))
-l1 = [str(i) for i in l1]
+l1: List[int] = [i for i in list(range(10000))]
 random.shuffle(l1)
 
+def sort(l1: List[int] = l1 )-> List[int]:
 
-def sort(l1 = l1):  
+    if type(l1[0]) == int : 
+        maxl: int = len(str(max(l1)))
+        l1: List[str] = [str(i) for i in l1]
+    else: return  
+
     def d(l1=l1):
-        temp = [[] for _ in range(maxl)]
+        temp: List[List] = [[] for _ in range(maxl)]
         for i in l1: temp[len(i)-1].append(i)
 
         return temp 
@@ -24,19 +27,21 @@ def sort(l1 = l1):
 
 
     def f(x, dig ):
-        pars = [[] for _ in range(10)]
+        pars: List[List] = [[] for _ in range(10)]
         for m in range(dig):
 
             for i in x: pars[int(i[::-1][m])].append(i)
-            x = pars[0]+ pars[1]+ pars[2]+ pars[3]+ pars[4]+ pars[5]+ pars[6]+ pars[7]+ pars[8]+ pars[9] 
-            pars = [[] for _ in range(10)]
+            x: List[int] = pars[0]+ pars[1]+ pars[2]+ pars[3]+ pars[4]+ pars[5]+ pars[6]+ pars[7]+ pars[8]+ pars[9] 
+            pars: List[List] = [[] for _ in range(10)]
 
         return x
 
 
     for dig,i in enumerate(l1): l1[dig] = f(i,dig+1) 
-    for i in l1: [print(j) for j in i]
-    
-    return l1
+     
+    return [int(j) for i in l1 for j in i]
 
-(sort())
+
+
+print(sort())
+
